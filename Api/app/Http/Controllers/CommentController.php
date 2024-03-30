26af73c9202daba795463fcb21e8708429b6f93b
OZO <ozoo12405@gmail.com>
2024-03-30 11:57:29 +0700

    third commit

--- Api/app/Http/Controllers/CommentController.php
+++ Api/app/Http/Controllers/CommentController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comments;
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Products $product)
+    {
+        $request->validate([
+            'body' => 'required|string',
+        ]);
+
+        $comment = Comments::create([
+            'post_id' => $product->id,
+            'user_id' => $request->user()->id,
+            'body' => $request->body
+        ]);
+    }
 }
