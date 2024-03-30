1038c97badb93fc92c948458f1ed099acd4469f0
OZO <ozoo12405@gmail.com>
2024-03-30 11:16:23 +0700

    first commit

---
+++ Api/app/Models/Products.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = ['image','header','title','price','details','quantity','scale','category_type','published_at','active'];
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function rate()
+    {
+        return $this->hasMany(Rate::class);
+    }
+
+    public function vote()
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+}
